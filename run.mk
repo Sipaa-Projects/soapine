03559b2c0d1bd4566ea25eb013da04972e0588f7
lolguy91 <retek1544@gmail.com>
2024-07-06 08:41:07 +0000

    please stop

--- run.mk
+++ run.mk
@@ -1,9 +1,9 @@
 ovmf-bin: 
 	@mkdir -p ovmf
 	@cd ovmf && curl -o OVMF-x64.fd https://retrage.github.io/edk2-nightly/bin/RELEASEX64_OVMF.fd
-	@cd ovmf && curl -o OVMF-aarch64.fd https://retrage.github.io/edk2-nightly/bin/RELEASEAARCH64_QEMU_EFI.fd
-	@cd ovmf && curl -o OVMF-riscv64.fd https://retrage.github.io/edk2-nightly/bin/RELEASERISCV64_VIRT_CODE.fd && dd if=/dev/zero of=OVMF-riscv64.fd bs=1 count=0 seek=33554432
-	@cd ovmf && curl -o OVMF-ia32.fd https://retrage.github.io/edk2-nightly/bin/RELEASEIa32_OVMF.fd
+# @cd ovmf && curl -o OVMF-aarch64.fd https://retrage.github.io/edk2-nightly/bin/RELEASEAARCH64_QEMU_EFI.fd
+# @cd ovmf && curl -o OVMF-riscv64.fd https://retrage.github.io/edk2-nightly/bin/RELEASERISCV64_VIRT_CODE.fd && dd if=/dev/zero of=OVMF-riscv64.fd bs=1 count=0 seek=33554432
+# @cd ovmf && curl -o OVMF-ia32.fd https://retrage.github.io/edk2-nightly/bin/RELEASEIa32_OVMF.fd
 
 run-uefi-x86_64: $(BIN).efi ovmf-bin
 	@mkdir -p $(BINDIR)/mnt
